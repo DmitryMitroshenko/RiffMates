c368320bd791e6ee7f86a1d7c523ced3adb2c9fd
DmitryMitroshenko <99461464+DmitryMitroshenko@users.noreply.github.com>
2025-05-22 03:36:56 +0700

    add musicians pagination

--- bands/urls.py
+++ bands/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from bands import views
 
 urlpatterns = [
-    path('musician/<int:musician_id>/', views.musician, name="musician")
+    path("musician/<int:musician_id>", views.musician, name="musician"),
+    path("musicians/", views.musicians, name="musicians"),
 ]
-
 
